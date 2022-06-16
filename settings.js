155857de25e83fc4c303f97f0d33fbdf3b721bdd
Tóth Ambrus <32463042+tothambrus11@users.noreply.github.com>
2022-06-16 23:58:54 +0200

    Delete own credentials
    Changed my password since then, don't worry :D

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 // DON'T FORGET TO ADJUST YOUR SAVED SEARCH SETTINGS, AND SET A SAVED MESSAGE! ./README.md
 
 export default {
-    USERNAME: "palpista11@gmail.com", // kamernet email
-    PASSWORD: "wL7u39@ACS7kvwM", // kamernet password
+    USERNAME: "", // kamernet email
+    PASSWORD: "", // kamernet password
     CITY: "delft", // the city name you are looking for (this should be in the url when you perform a search
 
     TIME_INTERVAL_MINUTES: 2, // time interval in which it sends messages (min),
